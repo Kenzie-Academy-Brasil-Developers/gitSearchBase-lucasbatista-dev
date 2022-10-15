65b8155708a9fdd4d334a08b7acf747968c22f4c
lucasbatista-dev <lucas_pivatto@hotmail.com.br>
2022-10-15 17:52:55 -0300

    first commit

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,41 @@
-/* Desenvolva sua lógica aqui...*/
+// ---------------------------------> PEGANDO ELEMENTOS HTML
+// const inputSearch = document.querySelector("#searchUser");
+
+// async function getGitAPI(baseUrl) {
+//   const data = await fetch(baseUrl);
+//   const dataJson = await data.json();
+
+//   return dataJson;
+// }
+
+// async function getAUser() {
+//   const baseUrl = `https://api.github.com/users/${inputSearch.value}`;
+//   const valueSearch = await getGitAPI(baseUrl);
+//   console.log(valueSearch);
+// }
+// getAUser();
+
+function next() {
+  const button = document.querySelector(".button-profile");
+
+  button.addEventListener("click", () => {
+    window.location.replace("pages/profile/index.html");
+  });
+}
+next();
+
+function search() {
+  const button = document.querySelector(".button-profile");
+
+  button.addEventListener("click", () => {
+    button.innerHTML = "";
+
+    const img = document.createElement("img");
+    img.src = "/assets/spinner.svg";
+    img.alt = "spinner";
+    img.classList.add("loading");
+
+    button.append(img);
+  });
+}
+search();
