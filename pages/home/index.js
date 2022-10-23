aa74199919bdf41a4a492cd77ccc1e90834a0ec5
Lucas Batista da Silva <lucas_pivatto@hotmail.com.br>
2022-10-23 19:46:21 -0300

    add localStorage

--- pages/home/index.js
+++ pages/home/index.js
@@ -2,6 +2,19 @@
 const url = `https://api.github.com/`;
 const inputSearch = document.querySelector(".input-search");
 const buttonSearch = document.querySelector(".button-profile");
+const msgError = document.querySelector(".notFound");
+
+inputSearch.addEventListener("input", () => {
+  msgError.classList = "notFound";
+  buttonSearch.disabled = false;
+  buttonSearch.style.color = "var(--color-grey-7)";
+  buttonSearch.style.background = "var(--color-brand-2)";
+  if (inputSearch.value.length == 0) {
+    buttonSearch.disabled = true;
+    buttonSearch.style.background = "rgba(214, 51, 108, 0.4)";
+    buttonSearch.style.color = "rgba(255, 255, 255, 0.35);";
+  }
+});
 
 console.log(inputSearch.value);
 
@@ -17,14 +30,16 @@ async function getAPIGit() {
 
   if (data.message == "Not Found") {
     setTimeout(() => {
-      const msgError = document.querySelector(".notFound");
       msgError.classList = "notFoundError";
       inputSearch.value = "";
       buttonSearch.innerHTML = "";
       buttonSearch.innerText = "Buscar na API";
+      buttonSearch.disabled = true;
+      buttonSearch.style.background = "rgba(214, 51, 108, 0.4)";
     }, 2000);
   } else {
     setTimeout(() => {
+      setRecentUser(data.login, data.avatar_url);
       inputSearch.value = "";
       window.location.href = `../profile/index.html?user=${data.login}`;
     }, 1000);
@@ -56,3 +71,57 @@ function search() {
     });
   });
 }
+
+// ---------------------------------> Usuarios recentes
+const ul = document.querySelector(".recentlyUserList");
+
+function createRecentUser(index, arrayRecently) {
+  ul.insertAdjacentHTML(
+    "afterbegin",
+    `
+      <li class="recentlyUsers">
+         <img
+         id="${index}"
+         class="recentlyImg"
+         src="${arrayRecently[index].urlImg}"
+         alt=""/>
+         <button class="buttonAccess">
+         <a href="/pages/profile/index.html?user=${arrayRecently[index].login}">Acessar este perfil</a>
+       </button>
+      </li>
+  
+  `
+  );
+}
+
+function setListRecently() {
+  let arrayRecently = JSON.parse(localStorage.recentUsers);
+  arrayRecently.reverse();
+  if (arrayRecently.length === 0) {
+  } else if (arrayRecently.length < 3) {
+    for (let index = 0; index < arrayRecently.length; index++) {
+      createRecentUser(index, arrayRecently);
+    }
+  } else {
+    for (let index = 0; index <= 2; index++) {
+      createRecentUser(index, arrayRecently);
+    }
+  }
+}
+
+function setRecentUser(login, avatar) {
+  let arrayRecently = JSON.parse(localStorage.recentUsers);
+  arrayRecently.push({ login: login, urlImg: avatar });
+  localStorage.recentUsers = JSON.stringify(arrayRecently);
+}
+
+function setLocalStorage() {
+  let arrayRecently = [];
+  if (localStorage.recentUsers) {
+  } else {
+    localStorage.recentUsers = JSON.stringify(arrayRecently);
+  }
+}
+
+setLocalStorage();
+setListRecently();
